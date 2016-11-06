ec46eb1ca3d7852e4f47f0ddd0c892f951894d73
Bartosz Bojda <barteboj@gmail.com>
2016-11-06 14:11:48 +0100

    Make cursor to be indicator in which direction user rotates camera

--- Virtual Tour/Assets/Scripts/CameraController.cs
+++ Virtual Tour/Assets/Scripts/CameraController.cs
@@ -1,16 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
     public Camera userCamera;
     public float cameraRotationSensitivity;
+    public Canvas mouseCursorCanvas;
+    public GameObject mouseCursorWhenRotatingCamera;
 
     void Update()
     {
         if (isUserRequestingCameraRotation)
         {
             RotateCameraAsUserRequested();
+            SetMouseCursorToIndicateCameraRotation();
+        }
+        else
+        {
+            SetMouseCursorToStandard();
         }
     }
 
@@ -51,5 +59,21 @@ public class CameraController : MonoBehaviour
         {
             return Vector2.Distance(Vector2.zero, new Vector2((Mathf.Clamp(Input.mousePosition.x, 0f, Screen.width) - Screen.width / 2f) / Screen.width, (Mathf.Clamp(Input.mousePosition.y, 0f, Screen.height) - Screen.height / 2f) / Screen.width)) * cameraRotationSensitivity;
         }
+    }
+
+    void SetMouseCursorToIndicateCameraRotation()
+    {
+        Vector3 newMousePos = new Vector3();
+        Cursor.visible = false;
+        mouseCursorWhenRotatingCamera.SetActive(true);
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(mouseCursorCanvas.GetComponent<RectTransform>(), new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.1f), null, out newMousePos);
+        mouseCursorWhenRotatingCamera.GetComponent<RectTransform>().position = newMousePos;
+        mouseCursorWhenRotatingCamera.transform.up = new Vector3(Mathf.Clamp(Input.mousePosition.x, 0f, Screen.width) - Screen.width / 2f, Mathf.Clamp(Input.mousePosition.y, 0f, Screen.height) - Screen.height / 2f).normalized;
+    }
+
+    void SetMouseCursorToStandard()
+    {
+        mouseCursorWhenRotatingCamera.SetActive(false);
+        Cursor.visible = true;
     }
 }
